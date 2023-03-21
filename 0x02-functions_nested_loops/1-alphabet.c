1f3cdae895b2aa94ea60f981bbeffda2be46861a
pleiadeslol <roumaissaezarhoune@gmail.com>
2023-03-21 07:24:56 -0700

    1

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,18 @@
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+/**
+ * print_alphabet - prints the alphabet
+ *
+ * Return: void.
+ */
+
+void print_alphabet(void)
+{
+	char alph ;
+	for (alph = 'a'; alph <= 'z'; alph++)
+		_putchar(alph);
+	_putchar('\n');
+}
