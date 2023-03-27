a8f7ac3747985dd8e79333698508cb69e219b809
pleiadeslol <roumaissaezarhoune@gmail.com>
2023-03-27 16:09:49 -0700

    5

---
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,23 @@
+#include "main.h"
+#include "2-strlen.c"
+
+/**
+ * rev_string - reverse a string
+ *
+ * @s: string
+ *
+ * Return: always 0
+ */
+
+void rev_string(char *s)
+{
+	int i;
+	char t;
+
+	for (i = 0; i < _strlen(s) / 2; i++)
+	{
+		t = s[i];
+		s[i] = s[_strlen(s) - i - 1];
+		s[_strlen(s) - i - 1] = t;
+	}
+}
