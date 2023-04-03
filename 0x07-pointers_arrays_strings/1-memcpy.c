17a74f73f71046a592673f21de328aa76d753cd2
pleiadeslol <roumaissaezarhoune@gmail.com>
2023-04-03 10:35:55 -0700

    fix

--- 0x07-pointers_arrays_strings/1-memcpy.c
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -15,7 +15,7 @@ char *_memcpy(char *dest, char *src, unsigned int n)
 	unsigned int i;
 	char *csrc = (char *)src;
 	char *cdest = (char *)dest;
-	
+
 	for (i = 0; i < n; i++)
 	{
 		cdest[i] = csrc[i];
