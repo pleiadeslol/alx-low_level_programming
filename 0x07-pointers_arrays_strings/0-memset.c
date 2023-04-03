1e8c99b12449a56546fb4ac512278eb11d6d2aa0
pleiadeslol <roumaissaezarhoune@gmail.com>
2023-04-03 09:27:01 -0700

    memset function

---
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ * _memset - copies char to the first n chars of string
+ *
+ * @s: pointer to the block of memory to fill
+ * @b: value to be set
+ * @n: number of bytes to be set to the value
+ *
+ * Return: pointer to the memory area
+ */
+
+char *_memset(char *s, char b, unsigned int n)
+{
+	unsigned int i = 0;
+
+	while (i <= n)
+	{
+		s[i] = b;
+		i++;
+	}
+	return (s);
+}
