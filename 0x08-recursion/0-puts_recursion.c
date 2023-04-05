a76f471685beb6ffe59eeec971d9e124115f70d2
pleiadeslol <roumaissaezarhoune@gmail.com>
2023-04-05 10:48:25 -0700

    fix

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -13,10 +13,10 @@ void _puts_recursion(char *s)
 	int i;
 
 	if (*s)
-    {
-        _putchar(*s);
-        _puts_recursion(++s);
-    }
-    else
-        _putchar('\n');
+	{
+		_putchar(*s);
+		_puts_recursion(++s);
+	}
+	else
+		_putchar('\n');
 }
