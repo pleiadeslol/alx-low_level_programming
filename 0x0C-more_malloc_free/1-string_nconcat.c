1f255d874df9390269fc57254ee38cdfbd1fc504
pleiadeslol <roumaissaezarhoune@gmail.com>
2023-04-13 16:59:53 -0700

    string n concat function

---
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -0,0 +1,48 @@
+#include "main.h"
+#include <stdlib.h>
+
+/**
+ * string_nconcat - function that concetanates two strings
+ *
+ * @s1: first string
+ * @s2: second string
+ * @n: size to be concetanated
+ *
+ * Return: pointer to a new allocated space in memo containing
+ * s1 followed by the first n bytes of s2, or NULL if func fail
+ */
+
+char *string_nconcat(char *s1, char *s2, unsigned int n)
+{
+	char *new_str;
+	unsigned int i = 0, lens1 = 0, lens2 = 0;
+
+	if (s1 == NULL)
+		s1 = "";
+
+	while (s1[lens1])
+		lens1++;
+
+	if (s2 == NULL)
+		s2 = "";
+
+	while (s2[lens2])
+		lens2++;
+
+	if (n >= lens2)
+		n = lens2;
+
+	new_str = malloc(lens1 + n + 1);
+	if (new_str == NULL)
+		return (NULL);
+
+	for (; i < (lens1 + n); i++)
+	{
+		if (i < lens1)
+			new_str[i] = *s1, s1++;
+		else
+			new_str[i] = *s2, s2++;
+	}
+	new_str[i] = '\0';
+	return (new_str);
+}
