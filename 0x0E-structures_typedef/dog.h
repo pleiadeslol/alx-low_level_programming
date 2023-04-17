1b12c974cc94419aad8428e6b14467844affd4a3
pleiadeslol <roumaissaezarhoune@gmail.com>
2023-04-17 09:58:01 -0700

    dog struct

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,19 @@
+#ifndef DOG_H
+#define DOG_H
+
+/**
+ * struct dog - dog informations
+ * @name: dog name
+ * @age: age of the dog
+ * @owner: owner of the dog
+ *
+ * Description: Longer description
+ */
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+
+#endif
