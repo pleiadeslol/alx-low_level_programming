4f0576af1e7bde590c50c7980f18d02394fc2eb8
pleiadeslol <roumaissaezarhoune@gmail.com>
2023-04-27 16:14:43 -0700

    task 0

--- 0x11-printf/main.h
+++
@@ -1,8 +0,0 @@
-#ifndef PRINTF_MAIN_H
-#define PRINTF_MAIN_H
-
-/* -- Functions -- */
-
-void print_char(va_list, format_t, void *);
-void _putchar(char, void *);
-int _printf(const char *format, ...);
