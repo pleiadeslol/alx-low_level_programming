d5204aabedb55f820b8b77871e827073bb0865a6
pleiadeslol <roumaissaezarhoune@gmail.com>
2023-04-28 08:31:30 -0700

    task 0

--- 0x13-more_singly_linked_lists/lists.h
+++ 0x13-more_singly_linked_lists/lists.h
@@ -18,10 +18,25 @@ typedef struct list_s
 	struct list_s *next;
 } list_t;
 
+/**
+ * struct listint_s - singly linked list
+ * @n: integer
+ * @next: points to the next node
+ *
+ * Description: singly linked list node structure
+ * 
+ */
+typedef struct listint_s
+{
+    int n;
+    struct listint_s *next;
+} listint_t;
+
 size_t print_list(const list_t *h);
 size_t list_len(const list_t *h);
 list_t *add_node(list_t **head, const char *str);
 list_t *add_node_end(list_t **head, const char *str);
 void free_list(list_t *head);
+size_t print_listint(const listint_t *h);
 
 #endif /* LISTS_H */
