aba1314a634cd6bb68ee514610e1719be41fd5d3
roumaissae zarhoune <115590428+pleiadeslol@users.noreply.github.com>
2023-04-28 01:19:45 +0000

    Update 0-print_list.c

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -15,12 +15,12 @@ size_t print_list(const list_t *h)
 {
     size_t count = 0;
 
-	while (h != NULL)
+    while (h)
 	{
-		if (h->str == NULL)
-			printf("[0] (nil)\n");
+		if (h->str)
+			printf("[%u] %s\n", h->len, h->str);
 		else
-			printf("[%d] %s\n", h->len, h->str);
+			printf("[0] (nil)\n");
 		h = h->next;
 		count++;
 	}
