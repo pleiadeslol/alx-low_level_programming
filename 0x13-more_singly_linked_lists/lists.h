3a78ed487747aa2813725e7f2a90eccc30906e92
pleiadeslol <roumaissaezarhoune@gmail.com>
2023-04-29 07:24:56 -0700

    fix betty style

--- 0x13-more_singly_linked_lists/lists.h
+++ 0x13-more_singly_linked_lists/lists.h
@@ -24,12 +24,12 @@ typedef struct list_s
  * @next: points to the next node
  *
  * Description: singly linked list node structure
- * 
+ *
  */
 typedef struct listint_s
 {
-    int n;
-    struct listint_s *next;
+	int n;
+	struct listint_s *next;
 } listint_t;
 
 size_t print_list(const list_t *h);
