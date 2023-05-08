1eb61a20d670d2699af15af6494c5b4df4f8af3d
pleiadeslol <roumaissaezarhoune@gmail.com>
2023-05-08 13:46:26 -0700

    fix: forgot to close fd

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -21,7 +21,10 @@ int create_file(const char *filename, char *text_content)
 		return (-1);
 
 	if (text_content == NULL)
+	{
+		close(fd);
 		return (-1);
+	}
 
 	for (len = 0; text_content[len]; len++)
 		;
