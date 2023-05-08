10b360a46566a957edc8ce3739712824294face0
pleiadeslol <roumaissaezarhoune@gmail.com>
2023-05-08 13:47:40 -0700

    fix: wrong return value

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -23,7 +23,7 @@ int create_file(const char *filename, char *text_content)
 	if (text_content == NULL)
 	{
 		close(fd);
-		return (-1);
+		return (1);
 	}
 
 	for (len = 0; text_content[len]; len++)
