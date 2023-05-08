99e3c99cfeefecbb75dc4450835fea6b28a97080
pleiadeslol <roumaissaezarhoune@gmail.com>
2023-05-08 14:36:14 -0700

    fix return value

--- 0x15-file_io/2-append_text_to_file.c
+++ 0x15-file_io/2-append_text_to_file.c
@@ -23,7 +23,7 @@ int append_text_to_file(const char *filename, char *text_content)
 	if (text_content == NULL)
 	{
 		close(fd);
-		return (-1);
+		return (1);
 	}
 
 	for (len = 0; text_content[len]; len++)
